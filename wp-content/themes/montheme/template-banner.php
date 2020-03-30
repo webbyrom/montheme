1312d6ef67a5738b4d8f86760c3dc89d386f9cdc
web-byrom <contact@web-byrom.com>
2020-03-30 18:12:54 +0200

    création fichier template banner soucis a l'affichage

---
+++ wp-content/themes/montheme/template-banner.php
@@ -0,0 +1,24 @@
+<?php 
+/** Template Name: Page avec bannière
+ * Template post Type: page, post 
+ * 
+ */
+?>
+<?php get_header() ?>
+<!--- appel du header--->
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <!---declaration-->
+                <div class="banniere-page">
+                    <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width: 100%; height:auto;"> 
+                </div>
+
+                <h1><?php the_title() ?></h1>
+                <p>
+                        <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width:20%; height: auto;">
+                </p>
+
+                <?php the_content() ?>
+<?php endwhile;
+endif ?>
+<?php get_footer() ?>
