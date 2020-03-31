87fdadb75b019fb3c2e6db3637258e7116ce5a28
web-byrom <contact@web-byrom.com>
2020-03-31 11:35:38 +0200

    test mise en place banner

--- wp-content/themes/montheme/template-banner.php
+++ wp-content/themes/montheme/template-banner.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /** Template Name: Page avec bannière
  * Template post Type: page, post 
  * 
@@ -6,19 +7,20 @@
 ?>
 <?php get_header() ?>
 <!--- appel du header--->
+<div class="banniere-page">
+    <img src="<?php the_post_thumbnail('large'); ?>" alt="" style="width: 100%; height:auto;">
+</div>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <!---declaration-->
-                <div class="banniere-page">
-                    <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width: 100%; height:auto;"> 
-                </div>
+        <!---declaration-->
 
-                <h1><?php the_title() ?></h1>
-                <p>
-                        <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width:20%; height: auto;">
-                </p>
 
-                <?php the_content() ?>
+        <h1><?php the_title() ?></h1>
+        <p>
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width:20%; height: auto;">
+        </p>
+
+        <?php the_content() ?>
 <?php endwhile;
 endif ?>
 <?php get_footer() ?>
