0815cd09194f8a559109c7eaa9e0ab26c74c5d4e
web-byrom <contact@web-byrom.com>
2020-04-01 19:20:07 +0200

    update wp

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -56,8 +56,28 @@ function montheme_pagination() /* création de la fonction pour la pagination **
     echo '</ul>';
     echo '</nav>';
 }
+function montheme_init() {
+    register_taxonomy('sport', 'post', [
+        'labels' => [
+            'name' => 'Sport',
+            'singular_name'     => 'Sport',
+            'plural_name'       => 'Sports',
+            'search_items'      => 'Rechercher des sports',
+            'all_items'         => 'Tous les sports',
+            'edit_item'         => 'Editer le sport',
+            'update_item'       => 'Mettre à jour le sport',
+            'add_new_item'      => 'Ajouter un nouveau sport',
+            'new_item_name'     => 'Ajouter un nouveau sport',
+            'menu_name'         => 'Sport',
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true,
+        'show_admin_column' => true,
 
+    ]);
+}
 
+add_action('init', 'montheme_init');
 add_action('after_setup_theme', 'montheme_supports');/* pour utiliser la function montheme_supports()*/
 add_action('wp_enqueue_scripts', 'montheme_register_assets');/* pour utiliser la function montheme_register_assets()*/
 add_filter('wp_title', 'montheme_title');
--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,5 +1,14 @@
 <?php get_header() ?>
 <!--- appel du header--->
+<?php wp_list_categories(['taxonmy' => 'sport', 'title_li' => '']); ?>
+<?php $sports = get_terms(['taxonomy' => 'sport']); ?>
+<ul class="nav nav-pills">
+    <?php foreach($sports as $sport): ?>
+    <li class="nav-item">
+        <a href="<?=get_term_link($sport) ?>" class="nav-link <?= is_tax('sport', $sport->term_id) ? 'active' : '' ?>"><?= $sport->name ?></a>
+    </li>
+    <?php endforeach; ?>
+</ul>
 
 <?php if (have_posts()) : ?>
     <!---declaration-->
@@ -12,6 +21,11 @@
                     <div class="card-body">
                         <h5 class="card-title"><?php the_title() ?></h5>
                         <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <ul>
+                            <?php
+                            the_terms(get_the_ID(), 'sport', '<li>', '</li><li>', '</li>');
+                            ?>
+                        </ul>
                         <!--- insertion function wp dans html---->
                         <p class="card-text">
                             <?php the_content('En voir plus') ?>
